43f631c631226b0a0c3b4c2c5c0ebdb119516f67
이현승 <77336664+gesal03@users.noreply.github.com>
2023-09-17 03:28:10 +0900

    Update record.py

--- record.py
+++ record.py
@@ -26,7 +26,8 @@ def run():
         fps = 30.0
         width = 1920
         height = 1080
-
+        
+        print("video: ", capGoalLine.get(cv2.CAP_PROP_FPS),", ",capGoalLine.get(cv2.CAP_PROP_FRAME_WIDTH), ", ", capGoalLine.get(cv2.CAP_PROP_FRAME_HEIGHT))
         # 비디오 생성 객체 
         outGoalLine = cv2.VideoWriter('output/goalline.mp4', fourcc, fps, (width, height))  # 파일 이름, 코덱, 프레임 속도, 프레임 크기 설정    
         # outLeft = cv2.VideoWriter('output/left.mp4', fourcc, fps, (width, height))  # 파일 이름, 코덱, 프레임 속도, 프레임 크기 설정
