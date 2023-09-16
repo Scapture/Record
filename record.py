a53ff3ff378e8050384a0ca8c1d68de103eda222
이현승 <77336664+gesal03@users.noreply.github.com>
2023-09-17 04:16:57 +0900

    Update record.py

--- record.py
+++ record.py
@@ -18,7 +18,7 @@ def run():
     if recording:
         # 카메라 객체 생성, 변경
         # capGoalLine = cv2.VideoCapture(0)  # 0은 기본 웹캠을 나타냅니다. 다른 웹캠을 사용하려면 적절한 인덱스를 사용하세요.
-        capLeft = cv2.VideoCapture(0) 
+        capLeft = cv2.VideoCapture(1) 
         capRight = cv2.VideoCapture(2) 
 
         # 영상 초기 설정, fps,width, height 값을 적절하게 맞추어야 함.
