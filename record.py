f278a6135bfbf765a4c62b9a86ccced28122e6a5
이현승 <77336664+gesal03@users.noreply.github.com>
2023-09-17 03:25:56 +0900

    Update record.py

--- record.py
+++ record.py
@@ -18,8 +18,8 @@ def run():
     if recording:
         # 카메라 객체 생성, 변경
         capGoalLine = cv2.VideoCapture(0)  # 0은 기본 웹캠을 나타냅니다. 다른 웹캠을 사용하려면 적절한 인덱스를 사용하세요.
-        capLeft = cv2.VideoCapture(0) 
-        capRight = cv2.VideoCapture(1) 
+        # capLeft = cv2.VideoCapture(0) 
+        # capRight = cv2.VideoCapture(1) 
 
         # 영상 초기 설정, fps,width, height 값을 적절하게 맞추어야 함.
         fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 비디오 코덱 설정
@@ -29,14 +29,14 @@ def run():
 
         # 비디오 생성 객체 
         outGoalLine = cv2.VideoWriter('output/goalline.mp4', fourcc, fps, (width, height))  # 파일 이름, 코덱, 프레임 속도, 프레임 크기 설정    
-        outLeft = cv2.VideoWriter('output/left.mp4', fourcc, fps, (width, height))  # 파일 이름, 코덱, 프레임 속도, 프레임 크기 설정
-        outRight = cv2.VideoWriter('output/right.mp4', fourcc, fps, (width, height))  # 파일 이름, 코덱, 프레임 속도, 프레임 크기 설정
+        # outLeft = cv2.VideoWriter('output/left.mp4', fourcc, fps, (width, height))  # 파일 이름, 코덱, 프레임 속도, 프레임 크기 설정
+        # outRight = cv2.VideoWriter('output/right.mp4', fourcc, fps, (width, height))  # 파일 이름, 코덱, 프레임 속도, 프레임 크기 설정
 
         while True:
             # 프레임을 읽어옵니다.
             ret1, frame1 = capGoalLine.read()
-            ret2, frame2 = capLeft.read()
-            ret3, frame3 = capRight.read()
+            # ret2, frame2 = capLeft.read()
+            # ret3, frame3 = capRight.read()
 
             if cv2.waitKey(1) & 0xFF == ord('q'):  # 'q' 키를 누르면 루프를 종료합니다.
                 break
@@ -44,12 +44,12 @@ def run():
             if not ret1:
                 print("Failed GoalLine.")
                 break
-            if not ret2:
-                print("Failed left.")
-                break
-            if not ret3:
-                print("Failed right.")
-                break
+            # if not ret2:
+            #     print("Failed left.")
+            #     break
+            # if not ret3:
+            #     print("Failed right.")
+            #     break
             # stop_recording() 을 통해 recording이 False가 되면 종료
             if not recording:
                 print("record.py: stop")
@@ -63,14 +63,14 @@ def run():
 
             # 프레임을 녹화 파일에 추가합니다.
             outGoalLine.write(frame1)
-            outLeft.write(frame2)
-            outRight.write(frame3)
+            # outLeft.write(frame2)
+            # outRight.write(frame3)
 
         outGoalLine.release()
-        outLeft.release()
-        outRight.release()
+        # outLeft.release()
+        # outRight.release()
         capGoalLine.release()
-        capLeft.release()
-        capRight.release()
+        # capLeft.release()
+        # capRight.release()
         # FTP 파일 전송
         upload_file.run()
